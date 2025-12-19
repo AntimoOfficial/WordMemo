e2936f052eab9e6a94ae96bc05bc13a9a6c1f55d
AntimoOfficial <rju84034@gmail.com>
2025-12-19 16:30:02 +0800

    Initial Commit

---
+++ WordMemo/WordMemoApp.swift
@@ -0,0 +1,32 @@
+//
+//  WordMemoApp.swift
+//  WordMemo
+//
+//  Created by antimo on 2025/12/19.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WordMemoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
